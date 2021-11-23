5682213f5dc2aed3dd8c3496175df2bbee6566e4
Marina Ntogka <marinadoga98@gmail.com>
2021-11-23 14:54:33 +0200

    Add files via upload

---
+++ Beginner level - Java Fundamentals/ArraysApp.java
@@ -0,0 +1,32 @@
+public class ArraysApp {
+
+    public static void main(String[] args) {
+
+        int[] lotteryNumbers = new int[5];
+
+        lotteryNumbers[0] = 17;
+        lotteryNumbers[1] = 23;
+        lotteryNumbers[2] = 67;
+        lotteryNumbers[3] = 37;
+        lotteryNumbers[4] = 11;
+
+        for (int lotteryNumber : lotteryNumbers){
+            System.out.println(lotteryNumber);
+        }
+
+        int[][] weeklyLotteryNumber = {
+                {1, 2, 3, 4, 5},
+                {10, 20, 30, 40, 50},
+                {13, 24, 35, 46, 57},
+                {10, 23, 35, 48, 58}
+        };
+
+        for (int i=0; i < weeklyLotteryNumber.length; i++) {
+            for (int j = 0; j < weeklyLotteryNumber[i].length; j++) {
+                System.out.print(weeklyLotteryNumber[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ Beginner level - Java Fundamentals/WhoWantsToBeAMillionaireApp.java
@@ -0,0 +1,18 @@
+public class WhoWantsToBeAMillionaireApp {
+
+    public static void main(String[] args) {
+
+        char answer = 'C';
+
+        System.out.println("Who is named as the inventor of the light bulb?");
+        System.out.println("You marked " + answer + ". Let' s see whether you are right!");
+
+        if (answer == 'C') {
+            System.out.println("You were right, You won.");
+        } else {
+            System.out.println("Sorry, the right answer is Tomas Edison. Thanks for coming");
+        }
+
+    }
+
+}
