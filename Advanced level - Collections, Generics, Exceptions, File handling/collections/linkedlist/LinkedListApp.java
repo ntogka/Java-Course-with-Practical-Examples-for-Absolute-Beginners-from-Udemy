946c6cfb8130257404bd6efb021d1e667a06066a
Marina Ntogka <marinadoga98@gmail.com>
2021-11-25 15:33:39 +0200

    Add files via upload

---
+++ Advanced level - Collections, Generics, Exceptions, File handling/collections/linkedlist/LinkedListApp.java
@@ -0,0 +1,41 @@
+package advanced.collections.linkedlist;
+
+import java.util.LinkedList;
+import java.util.Locale;
+
+public class LinkedListApp {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> morningRoutine = new LinkedList<>();
+
+
+        if (!morningRoutine.isEmpty()) {
+            System.out.println("The first element of the list: " + morningRoutine.getFirst());
+        }
+
+        if (!morningRoutine.isEmpty() && morningRoutine.peekFirst() != null) {
+            System.out.println("The first element of the list: " + morningRoutine.peekFirst().toUpperCase());
+        }
+
+//        morningRoutine.add("Brushing my teeth");
+//        morningRoutine.add("Taking the dog for a walk");
+//        morningRoutine.add("Doing 32 push-ups");
+
+        morningRoutine.add("Brushing my teeth");
+        morningRoutine.addFirst("Taking the dog for a walk");
+        morningRoutine.push("Doing 32 push-ups");
+        morningRoutine.offer("Reading one chapter of a book");
+
+//        System.out.println("I used pop function: " + morningRoutine.pop());
+
+        System.out.println(morningRoutine);
+
+        System.out.println("I completed the first element on my list: " + morningRoutine.pollFirst());
+        System.out.println("I completed the last element on my list: " + morningRoutine.pollLast());
+
+        System.out.println(morningRoutine);
+
+        }
+
+}
